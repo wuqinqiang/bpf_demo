93b8c91c51e8cd800b443968b7810f45e60ea099
wuqinqiang <1185079673@qq.com>
2023-07-07 15:33:04 +0800

    add func demo

---
+++ func/func.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there from %s!", r.Host)
+}
+
+func main() {
+	http.HandleFunc("", handlerFunc)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
